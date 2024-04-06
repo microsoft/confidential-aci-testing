8f5d33a50c074293188c40b061e44632a2ccc83f
Dominic Ayre <domayre@outlook.com>
2024-04-06 11:14:05 +0000

    Fix env_create
    Signed-off-by: Dominic Ayre <domayre@outlook.com>

--- tools/env_create.py
+++ tools/env_create.py
@@ -4,15 +4,15 @@
 def env_create():
     print(
 """# Core Environment Variables
-SUBSCRIPTION=""
-RESOURCE_GROUP=""
-REGISTRY=""
-MANAGED_IDENTITY=""
-LOCATION=""
+SUBSCRIPTION
+RESOURCE_GROUP
+REGISTRY
+MANAGED_IDENTITY
+LOCATION
 
 # Per Target Options that can also be set at runtime
-# REPOSITORY=""
-# TAG="""""
+# REPOSITORY
+# TAG"""
 )
 
 if __name__ == "__main__":
