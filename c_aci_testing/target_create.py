eea25aff9b5a730c721a6ab686272be00a28d82b
Dominic Ayre <domayre@outlook.com>
2024-04-07 18:57:21 +0000

    .
    Signed-off-by: Dominic Ayre <domayre@outlook.com>

--- c_aci_testing/target_create.py
+++ c_aci_testing/target_create.py
@@ -4,23 +4,19 @@
 import argparse
 import os
 import subprocess
+from pkg_resources import resource_filename
 
 
 def target_create(target, name):
 
-    if not os.path.exists(target):
-        os.mkdir(target)
-
+    os.makedirs(target, exist_ok=True)
     if os.listdir(target):
         print(f"Target directory must be empty")
         return
     
-    os.makedirs(target, exist_ok=True)
     example_path = os.environ.get("EXAMPLE_PATH")
     if example_path is None:
-        example_path = os.path.realpath(
-            os.path.join(os.path.dirname(__file__), "..", "test", "example")
-        )
+        example_path = resource_filename(__name__, "../test/example/")
     
     # Copy the contents of example_path to target
     subprocess.run(["cp", "-r", example_path + "/.", target])
