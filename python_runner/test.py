d24ff367df32e985e36c0dd72696980dfd503009
Dominic Ayre <domayre@outlook.com>
2024-04-08 17:02:33 +0000

    Add script to get container IP and add to python runner
    Signed-off-by: Dominic Ayre <domayre@outlook.com>

--- python_runner/test.py
+++ python_runner/test.py
@@ -4,14 +4,14 @@
 import unittest
 import os
 
-from c_aci_testing.target_run import target_run
+from c_aci_testing.target_run import target_run, aci_get_ips
 
 class ExampleTest(unittest.TestCase):
     def test_example(self):
         
         target_dir = os.path.realpath(os.path.dirname(__file__))
         with target_run(target_dir, "my-deployment") as deployment_id:
-            ... # Do something with the deployment
+            print(f"Executing test body, container group IP: {aci_get_ips(ids=deployment_id)}")
 
         # Cleanup happens after block has finished
 
