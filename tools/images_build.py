2eeed6ba048507570b2dc75309cc1665cf3d2aec
Dominic Ayre <domayre@outlook.com>
2024-04-09 17:13:54 +0000

    Add docker-compose support
    Signed-off-by: Dominic Ayre <domayre@outlook.com>

--- tools/images_build.py
+++ tools/images_build.py
@@ -7,24 +7,20 @@ import argparse
 import os
 import subprocess
 
-from .target_find_files import find_bicep_file
-
-def images_build(target, registry, repository, tag="latest"):
+def images_build(target, registry, tag="latest"):
 
     assert target is not None, "Target is required"
     assert registry is not None, "Registry is required"
-    if repository is None:
-        repository = os.path.splitext(find_bicep_file(target))[0]
 
-    for dockerfile in os.listdir(target):
-        if dockerfile.endswith(".Dockerfile"):
-            image_name = os.path.splitext(dockerfile)[0]
-            subprocess.run([
-                "docker", "build",
-                "-t", f'{registry}/{repository}/{image_name}:{tag}',
-                "-f", f"{target}/{dockerfile}",
-                target,
-            ])
+    subprocess.run(["docker-compose", "build"],
+        env={
+            **os.environ,
+            "REGISTRY": registry,
+            "TAG": tag,
+        },
+        cwd=target,
+        check=True,
+    )
 
 
 if __name__ == "__main__":
@@ -36,8 +32,6 @@ if __name__ == "__main__":
         type=lambda path: os.path.abspath(os.path.expanduser(path)))
     parser.add_argument("--registry",
         help="Container Registry", default=os.environ.get("REGISTRY"))
-    parser.add_argument("--repository",
-        help="Container Repository", default=os.environ.get("REPOSITORY"))
     parser.add_argument("--tag",
         help="Image Tag", default=os.environ.get("TAG") or "latest")
 
@@ -46,6 +40,5 @@ if __name__ == "__main__":
     images_build(
         target=args.target,
         registry=args.registry,
-        repository=args.repository,
         tag=args.tag,
     )
--- tools/images_push.py
+++ tools/images_push.py
@@ -7,25 +7,19 @@ import argparse
 import os
 import subprocess
 
-from .target_find_files import find_bicep_file
+def images_push(target, registry, tag):
 
-def images_push(target, registry, repository, tag):
+    subprocess.run(["az", "acr", "login", "--name", registry])
 
-    if repository is None:
-        repository = os.path.splitext(find_bicep_file(target))[0]
-
-    subprocess.run([
-        "az", "acr", "login",
-        "--name", registry,
-    ])
-
-    for dockerfile in os.listdir(target):
-        if dockerfile.endswith(".Dockerfile"):
-            image_name = os.path.splitext(dockerfile)[0]
-            subprocess.run([
-                    "docker", "push",
-                    f'{registry}/{repository}/{image_name}:{tag}',
-                ])
+    subprocess.run(["docker-compose", "push"],
+        env={
+            **os.environ,
+            "REGISTRY": registry,
+            "TAG": tag,
+        },
+        cwd=target,
+        check=True,
+    )
 
 
 if __name__ == "__main__":
@@ -37,8 +31,6 @@ if __name__ == "__main__":
         type=lambda path: os.path.abspath(os.path.expanduser(path)))
     parser.add_argument("--registry",
         help="Container Registry", default=os.environ.get("REGISTRY"))
-    parser.add_argument("--repository",
-        help="Container Repository", default=os.environ.get("REPOSITORY"))
     parser.add_argument("--tag",
         help="Image Tag", default=os.environ.get("TAG") or "latest")
 
