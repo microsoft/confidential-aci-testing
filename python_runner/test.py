107780adaae4e8e2b1f0794bd34c07c19ce7bfd6
Dominic Ayre <domayre@outlook.com>
2024-04-09 09:07:57 +0000

    Update example runner to explicitly follow container
    Signed-off-by: Dominic Ayre <domayre@outlook.com>

--- python_runner/test.py
+++ python_runner/test.py
@@ -11,7 +11,7 @@ class ExampleTest(unittest.TestCase):
     def test_example(self):
         
         target_dir = os.path.realpath(os.path.dirname(__file__))
-        with target_run(target_dir, "my-deployment") as deployment_id:
+        with target_run(target_dir, "my-deployment", follow=True) as deployment_id:
             print(f"Executing test body, container group IP: {aci_get_ips(ids=deployment_id)}")
 
         # Cleanup happens after block has finished
