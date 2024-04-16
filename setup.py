2beba18411f63ce7ed392ee58392ea4d48dea811
Dominic Ayre <dominicayre@microsoft.com>
2024-04-16 11:02:50 +0000

    Update version

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='c-aci-testing',
-    version='0.1.1',
-    description='Utilities for testing workflows involving Confidential ACI.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Dominic Ayre',
-    author_email='dominicayre@microsoft.com',
-    url='https://github.com/microsoft/c-aci-testing',
+    name="c-aci-testing",
+    version="0.1.2",
+    description="Utilities for testing workflows involving Confidential ACI.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Dominic Ayre",
+    author_email="dominicayre@microsoft.com",
+    url="https://github.com/microsoft/c-aci-testing",
     packages=find_packages(),
     install_requires=[
         "pytest",
@@ -16,12 +16,12 @@ setup(
     ],
     include_package_data=True,
     package_data={
-        '': [
-            './test/example/*',
-            './aci/*',
-            './.vscode/*',
-            './python_runner/*',
-            './github_actions/*',
+        "": [
+            "./test/example/*",
+            "./aci/*",
+            "./.vscode/*",
+            "./python_runner/*",
+            "./github_actions/*",
         ],
     },
 )
