c913e1758e2bafcf09bb15d599cf80495eb4435f
Dominic Ayre <dominicayre@microsoft.com>
2024-04-16 11:02:23 +0000

    Respect tag in deployments

--- tools/aci_deploy.py
+++ tools/aci_deploy.py
@@ -10,7 +10,16 @@ from .aci_param_set import aci_param_set
 from .target_find_files import find_bicep_file, find_bicep_param_file
 
 
-def aci_deploy(target, subscription, resource_group, name, location, managed_identity, parameters):
+def aci_deploy(
+    target,
+    subscription,
+    resource_group,
+    name,
+    location,
+    tag,
+    managed_identity,
+    parameters,
+):
 
     assert target is not None, "Target is required"
     assert resource_group is not None, "Resource Group is required"
@@ -20,52 +29,94 @@ def aci_deploy(target, subscription, resource_group, name, location, managed_ide
         aci_param_set(param_file_path, "location", location)
     if managed_identity is not None:
         aci_param_set(param_file_path, "managedIDName", managed_identity)
+    if tag is not None:
+        aci_param_set(param_file_path, "tag", tag)
 
     az_command = [
-        "az", "deployment", "group", "create",
-        "-n", name,
+        "az",
+        "deployment",
+        "group",
+        "create",
+        "-n",
+        name,
         *(["--subscription", subscription] if subscription else []),
-        "--resource-group", resource_group,
-        "--template-file", os.path.join(target, find_bicep_file(target)),
-        "--parameters", param_file_path,
-        "--query", "properties.outputs.id.value", "-o", "tsv",
+        "--resource-group",
+        resource_group,
+        "--template-file",
+        os.path.join(target, find_bicep_file(target)),
+        "--parameters",
+        param_file_path,
+        "--query",
+        "properties.outputs.id.value",
+        "-o",
+        "tsv",
     ]
     for parameter in parameters or []:
         key = parameter.split("=")[0]
-        value = "=".join(parameter.split("=")[1:]).strip("\"")
-        az_command.extend(["--parameters", f'{key}={value}'])
+        value = "=".join(parameter.split("=")[1:]).strip('"')
+        az_command.extend(["--parameters", f"{key}={value}"])
 
     subprocess.run(az_command, check=True)
 
-    return subprocess.run([
-        "az", "deployment", "group", "show",
-        "--name", name,
-        *(["--subscription", subscription] if subscription else []),
-        "--resource-group", resource_group,
-        "--query", "properties.outputs.id.value", "-o", "tsv"
-    ], check=True, capture_output=True, text=True).stdout.rstrip("\n")
+    return subprocess.run(
+        [
+            "az",
+            "deployment",
+            "group",
+            "show",
+            "--name",
+            name,
+            *(["--subscription", subscription] if subscription else []),
+            "--resource-group",
+            resource_group,
+            "--query",
+            "properties.outputs.id.value",
+            "-o",
+            "tsv",
+        ],
+        check=True,
+        capture_output=True,
+        text=True,
+    ).stdout.rstrip("\n")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Deploy ACI for target")
 
-    parser.add_argument("target",
-        help="Target directory", default=os.environ.get("TARGET"),
-        nargs="?", # aka Optional
-        type=lambda path: os.path.abspath(os.path.expanduser(path)))
+    parser.add_argument(
+        "target",
+        help="Target directory",
+        default=os.environ.get("TARGET"),
+        nargs="?",  # aka Optional
+        type=lambda path: os.path.abspath(os.path.expanduser(path)),
+    )
 
     # Azure Information
-    parser.add_argument("--subscription",
-        help="Azure Subscription ID", default=os.environ.get("SUBSCRIPTION"))
-    parser.add_argument("--resource-group", '-g',
-        help="Azure Resource Group ID", default=os.environ.get("RESOURCE_GROUP"))
+    parser.add_argument(
+        "--subscription",
+        help="Azure Subscription ID",
+        default=os.environ.get("SUBSCRIPTION"),
+    )
+    parser.add_argument(
+        "--resource-group",
+        "-g",
+        help="Azure Resource Group ID",
+        default=os.environ.get("RESOURCE_GROUP"),
+    )
+    parser.add_argument(
+        "--tag", help="Image Tag", default=os.environ.get("TAG") or "latest"
+    )
 
     # Deployment Info
     parser.add_argument("--name", "-n", help="Name of deployment", required=True)
-    parser.add_argument("--location",
-        help="Location to deploy to", default=os.environ.get("LOCATION"))
-    parser.add_argument("--managed-identity",
-        help="Managed Identiy", default=os.environ.get("MANAGED_IDENTITY"))
+    parser.add_argument(
+        "--location", help="Location to deploy to", default=os.environ.get("LOCATION")
+    )
+    parser.add_argument(
+        "--managed-identity",
+        help="Managed Identiy",
+        default=os.environ.get("MANAGED_IDENTITY"),
+    )
     parser.add_argument("--parameters", help="Path to parameters file", action="append")
 
     args = parser.parse_args()
@@ -78,4 +129,5 @@ if __name__ == "__main__":
         location=args.location,
         managed_identity=args.managed_identity,
         parameters=args.parameters,
+        tag=args.tag,
     )
--- tools/target_run.py
+++ tools/target_run.py
@@ -13,6 +13,7 @@ from .aci_deploy import aci_deploy
 from .aci_monitor import aci_monitor
 from .aci_remove import aci_remove
 
+
 @contextmanager
 def target_run_ctx(
     target,
@@ -26,7 +27,7 @@ def target_run_ctx(
     parameters=None,
     follow=True,
     cleanup=True,
-    repository=None
+    repository=None,
 ):
 
     images_build(
@@ -55,6 +56,7 @@ def target_run_ctx(
         name=name,
         location=location,
         managed_identity=managed_identity,
+        tag=tag,
         parameters=parameters,
     )
     print("Deployment complete")
@@ -74,43 +76,71 @@ def target_run_ctx(
                 subscription=subscription,
                 resource_group=resource_group,
                 name=None,
-                ids=id
+                ids=id,
             )
 
+
 def target_run(**kwargs):
-    with target_run_ctx(**kwargs): ...
+    with target_run_ctx(**kwargs):
+        ...
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generate Security Policies for target")
+    parser = argparse.ArgumentParser(
+        description="Generate Security Policies for target"
+    )
 
-    parser.add_argument("target",
-        help="Target directory", default=os.environ.get("TARGET"),
-        nargs="?", # aka Optional
-        type=lambda path: os.path.abspath(os.path.expanduser(path)))
+    parser.add_argument(
+        "target",
+        help="Target directory",
+        default=os.environ.get("TARGET"),
+        nargs="?",  # aka Optional
+        type=lambda path: os.path.abspath(os.path.expanduser(path)),
+    )
 
     # Image Information
-    parser.add_argument("--registry",
-        help="Container Registry", default=os.environ.get("REGISTRY"))
-    parser.add_argument("--repository",
-        help="Container Repository", default=os.environ.get("REPOSITORY"))
-    parser.add_argument("--tag",
-        help="Image Tag", default=os.environ.get("TAG") or "latest")
+    parser.add_argument(
+        "--registry", help="Container Registry", default=os.environ.get("REGISTRY")
+    )
+    parser.add_argument(
+        "--repository",
+        help="Container Repository",
+        default=os.environ.get("REPOSITORY"),
+    )
+    parser.add_argument(
+        "--tag", help="Image Tag", default=os.environ.get("TAG") or "latest"
+    )
 
     # Azure Information
-    parser.add_argument("--subscription",
-        help="Azure Subscription ID", default=os.environ.get("SUBSCRIPTION"))
-    parser.add_argument("--resource-group", '-g',
-        help="Azure Resource Group ID", default=os.environ.get("RESOURCE_GROUP"))
+    parser.add_argument(
+        "--subscription",
+        help="Azure Subscription ID",
+        default=os.environ.get("SUBSCRIPTION"),
+    )
+    parser.add_argument(
+        "--resource-group",
+        "-g",
+        help="Azure Resource Group ID",
+        default=os.environ.get("RESOURCE_GROUP"),
+    )
 
     # Deployment Info
     parser.add_argument("--name", "-n", help="Name of deployment", required=True)
-    parser.add_argument("--location",
-        help="Location to deploy to", default=os.environ.get("LOCATION"))
-    parser.add_argument("--managed-identity",
-        help="Managed Identiy", default=os.environ.get("MANAGED_IDENTITY"))
+    parser.add_argument(
+        "--location", help="Location to deploy to", default=os.environ.get("LOCATION")
+    )
+    parser.add_argument(
+        "--managed-identity",
+        help="Managed Identiy",
+        default=os.environ.get("MANAGED_IDENTITY"),
+    )
     parser.add_argument("--parameters", help="Path to parameters file")
-    parser.add_argument("--no-cleanup", help="Path to parameters file", action="store_true")
-    parser.add_argument("--no-follow", help="Path to parameters file", action="store_true")
+    parser.add_argument(
+        "--no-cleanup", help="Path to parameters file", action="store_true"
+    )
+    parser.add_argument(
+        "--no-follow", help="Path to parameters file", action="store_true"
+    )
 
     args = parser.parse_args()
 
