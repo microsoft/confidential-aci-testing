7856392f3c8142d6a6898c9987a6c869a72a224b
Dominic Ayre <dominicayre@microsoft.com>
2024-04-20 09:46:01 +0100

    Add python non-terminating test (#26)
    Signed-off-by: Dominic Ayre <dominicayre@microsoft.com>

---
+++ test/example-non-terminating/test.py
@@ -0,0 +1,29 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import unittest
+import os
+import uuid
+
+from c_aci_testing.target_run import target_run_ctx
+from c_aci_testing.aci_get_ips import aci_get_ips
+
+class ExampleTest(unittest.TestCase):
+    def test_example(self):
+
+        target_dir = os.path.realpath(os.path.dirname(__file__))
+        id = str(uuid.uuid4())
+
+        with target_run_ctx(
+            target=target_dir,
+            name=f"my-deployment-{id}",
+            tag=id,
+            follow=False
+        ) as deployment_ids:
+            print(f"Executing test body, container group IP: {aci_get_ips(ids=deployment_ids[0])}")
+
+        # Cleanup happens after block has finished
+
+
+if __name__ == "__main__":
+    unittest.main()
