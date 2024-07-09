f58e1ad862b764e2c189825c32cb934e3d15f101
Dominic Ayre <dominicayre@microsoft.com>
2024-07-09 14:40:27 +0000

    Set version

--- src/c_aci_testing/__init__.py
+++ src/c_aci_testing/__init__.py
@@ -2,7 +2,6 @@
 #   Copyright (c) Microsoft Corporation. All rights reserved.
 #   Licensed under the MIT License. See LICENSE in project root for information.
 #   -------------------------------------------------------------
-"""Python Package Template"""
 from __future__ import annotations
 
-__version__ = "0.0.2"
+__version__ = "1.0.0"
