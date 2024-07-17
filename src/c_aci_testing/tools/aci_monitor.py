7029fd15a085e34da85bfbb95cc1eeb2f73c3524
Dominic Ayre <dominicayre@microsoft.com>
2024-07-17 17:27:43 +0100

    feat: get logs from all containers in group (#60)

--- src/c_aci_testing/tools/aci_monitor.py
+++ src/c_aci_testing/tools/aci_monitor.py
@@ -6,6 +6,7 @@
 from __future__ import annotations
 
 import subprocess
+import json
 
 from .aci_get_ids import aci_get_ids
 
@@ -19,11 +20,23 @@ def aci_monitor(
 ):
     for id in aci_get_ids(deployment_name, subscription, resource_group):
         group_name = id.split("/")[-1]
-        print(f"Logs from {group_name}")
-        subprocess.run([
-            "az", "container", "logs",
-            *(["--follow"] if follow else []),
-            "--subscription", subscription,
-            "--resource-group", resource_group,
-            "--name", group_name,
-        ], check=not follow)
+        res = subprocess.run(
+            [
+                "az", "container", "show",
+                "--name", group_name,
+                "--subscription", subscription,
+                "--resource-group", resource_group,
+            ],
+            stdout=subprocess.PIPE,
+        )
+        group_json = json.loads(res.stdout)
+        for container_json in group_json["containers"]:
+            print(f"Logs from {group_name} - {container_json['name']}")
+            subprocess.run([
+                "az", "container", "logs",
+                *(["--follow"] if follow else []),
+                "--subscription", subscription,
+                "--resource-group", resource_group,
+                "--name", group_name,
+                "--container-name", container_json["name"]
+            ], check=not follow)
