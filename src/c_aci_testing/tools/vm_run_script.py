37fe219bb1f69c20cbc84f0b157945085589efeb
Tingmao Wang <tingmaowang@microsoft.com>
2024-10-17 11:29:16 +0100

    fix run_script blob name

--- src/c_aci_testing/tools/vm_run_script.py
+++ src/c_aci_testing/tools/vm_run_script.py
@@ -24,15 +24,19 @@ def vm_run_script(
     if "/" in script_file or "\\" in script_file:
         raise ValueError("script_file must be a file name within target_path, not a path")
 
+    dir_name = path.split(target_path)[-1]
+    if not dir_name:
+        dir_name = "run_script"
+
     upload_to_vm_and_run(
         target_path=target_path,
-        vm_path="C:\\" + path.split(target_path)[-1],
+        vm_path="C:\\" + dir_name,
         vm_name=f"{deployment_name}-vm",
         subscription=subscription,
         resource_group=resource_group,
         storage_account=storage_account,
         container_name="container",
-        blob_name=f"{deployment_name}_vm_run_script_{path.dirname(target_path)}",
+        blob_name=f"{deployment_name}_vm_run_script_{dir_name}",
         managed_identity=managed_identity,
         run_script=script_file,
     )
