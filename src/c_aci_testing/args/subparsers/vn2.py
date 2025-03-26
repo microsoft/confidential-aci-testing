3b7a958244a679a64377dbed82f8782595f1bfff
Tingmao Wang <tingmaowang@microsoft.com>
2025-03-26 20:11:16 +0000

    draft vn2 deploy/remove/log

--- src/c_aci_testing/args/subparsers/vn2.py
+++ src/c_aci_testing/args/subparsers/vn2.py
@@ -16,16 +16,22 @@ from ..parameters.registry import parse_registry
 from ..parameters.repository import parse_repository
 from ..parameters.tag import parse_tag
 
+import os
+
 
 def subparse_vn2(vm: argparse.ArgumentParser):
 
     vn2_subparser = vm.add_subparsers(dest="vn2_command", required=True)
 
+    if os.getenv("DEPLOYMENT_NAME"):
+        yaml_path_default = f"{os.getenv('DEPLOYMENT_NAME')}.yaml"
+    else:
+        yaml_path_default = None
+
     generate_yaml = vn2_subparser.add_parser("generate_yaml")
     parse_deployment_name(generate_yaml)
     parse_managed_identity(generate_yaml)
     parse_target_path(generate_yaml)
-    generate_yaml.add_argument("output_yaml_path", type=str)
     parse_subscription(generate_yaml)
     parse_resource_group(generate_yaml)
     parse_registry(generate_yaml)
@@ -37,3 +43,26 @@ def subparse_vn2(vm: argparse.ArgumentParser):
         help="Number of replicas in the deployment template",
         default=1,
     )
+    generate_yaml.add_argument(
+        "--output-yaml-path",
+        type=str,
+        default=yaml_path_default,
+        help="Path to the YAML file to generate. If DEPLOYMENT_NAME environment variable is set, defaults to that.yaml",
+    )
+
+    # Deploy command
+    deploy = vn2_subparser.add_parser("deploy")
+    deploy.add_argument(
+        "--input-yaml-path",
+        type=str,
+        default=yaml_path_default,
+        help="Path to the YAML file to deploy. If DEPLOYMENT_NAME environment variable is set, defaults to that.yaml",
+    )
+
+    # Logs command
+    logs = vn2_subparser.add_parser("logs")
+    parse_deployment_name(logs)
+
+    # Remove command
+    remove = vn2_subparser.add_parser("remove")
+    parse_deployment_name(remove)
---
+++ src/c_aci_testing/tools/vn2_logs.py
@@ -0,0 +1,23 @@
+#   ---------------------------------------------------------------------------------
+#   Copyright (c) Microsoft Corporation. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   ---------------------------------------------------------------------------------
+
+from __future__ import annotations
+import subprocess
+
+
+def vn2_logs(deployment_name: str, **kwargs):
+    """
+    Fetch logs for all containers in the VN2 deployment.
+    """
+    # Construct the label selector for the deployment
+    label_selector = f"app=tingmao-vn2-{deployment_name}"
+
+    # Run kubectl logs command to fetch logs from all containers
+    subprocess.run(
+        ["kubectl", "logs", "--all-containers", "-l", label_selector, "--tail=-1"],
+        check=True
+    )
+
+    print(f"Displayed logs for VN2 deployment: {deployment_name}")
