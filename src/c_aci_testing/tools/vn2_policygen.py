889da924646d9900d0387e6df720df13c6cd5231
Tingmao Wang <tingmaowang@microsoft.com>
2025-08-18 11:15:03 +0100

    Fix vn2 policy generation
    Fix policy in yaml being inconsistent with the policy in the written rego, due
    to the rego file being prettied.  We now write the yaml ourselves, instead of
    calling confcom again.  This also lets us support allow_all policies.

--- src/c_aci_testing/tools/vn2_policygen.py
+++ src/c_aci_testing/tools/vn2_policygen.py
@@ -10,8 +10,17 @@ import re
 import subprocess
 import sys
 import os
+import yaml
+import base64
 
 from c_aci_testing.utils.find_bicep import find_bicep_files
+
+ALLOW_ALL_POLICY_REGO_PATH = os.path.join(
+    os.path.dirname(__file__),
+    "..",
+    "templates",
+    "allow_all_policy.rego",
+)
 
 
 def vn2_policygen(
@@ -30,35 +39,44 @@ def vn2_policygen(
 
     rego_output_path = Path(target_path) / f"policy_{bicep_file_name}.rego"
 
-    print("Calling acipolicygen and saving policy to file")
-    subprocess.run(["az", "extension", "add", "--name", "confcom", "--yes"], check=True)
-    args = [
-        "az",
-        "confcom",
-        "acipolicygen",
-        "--virtual-node-yaml",
-        yaml_path,
-        *(["--debug-mode"] if policy_type == "debug" else []),
-        *(["--include-fragments", "--fragments-json", fragments_json] if fragments_json else []),
-        *(["--infrastructure-svn", str(infrastructure_svn)] if infrastructure_svn is not None else []),
-        "--outraw-pretty-print",
-    ]
-    print("Running: " + " ".join(args), flush=True)
-    sys.stderr.flush()
-    res = subprocess.run(
-        args,
-        check=True,
-        stdout=subprocess.PIPE,
-    )
-    policy = res.stdout.decode()
+    if policy_type != "allow_all":
+        print("Calling acipolicygen and saving policy to file")
+        subprocess.run(["az", "extension", "add", "--name", "confcom", "--yes"], check=True)
+        args = [
+            "az",
+            "confcom",
+            "acipolicygen",
+            "--virtual-node-yaml",
+            yaml_path,
+            *(["--debug-mode"] if policy_type == "debug" else []),
+            *(["--include-fragments", "--fragments-json", fragments_json] if fragments_json else []),
+            *(["--infrastructure-svn", str(infrastructure_svn)] if infrastructure_svn is not None else []),
+            "--outraw-pretty-print",
+        ]
+        print("Running: " + " ".join(args), flush=True)
+        sys.stderr.flush()
+        res = subprocess.run(
+            args,
+            check=True,
+            stdout=subprocess.PIPE,
+        )
+        policy = res.stdout.decode()
+    else:
+        with open(ALLOW_ALL_POLICY_REGO_PATH, encoding="utf-8") as policy_file:
+            policy = policy_file.read()
     with open(rego_output_path, "wt") as f:
         f.write(policy)
     print(f"Policy written to {rego_output_path}")
 
-    args.pop()  # remove --outraw-pretty-print
-    sys.stderr.flush()
-    subprocess.run(
-        args,
-        check=True,
-    )
-    print(f"Policy applied to {yaml_path}")
+    with open(yaml_path, "rt") as f:
+        yaml_data = list(yaml.safe_load_all(f))
+        for r in yaml_data:
+            try:
+                if r["kind"] == "Deployment":
+                    r["spec"]["template"]["metadata"]["annotations"][
+                        "microsoft.containerinstance.virtualnode.ccepolicy"
+                    ] = base64.b64encode(policy.encode("utf8")).decode("utf8")
+            except KeyError:
+                continue
+        with open(yaml_path, "wt") as f:
+            yaml.dump_all(yaml_data, f)
