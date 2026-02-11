cd5a3a3ece2ded30d1814e4d11e0458763b8fba2
Tingmao Wang <tingmaowang@microsoft.com>
2026-02-11 15:09:57 +0000

    Generate createc.ps1

--- src/c_aci_testing/tools/vm_generate_scripts.py
+++ src/c_aci_testing/tools/vm_generate_scripts.py
@@ -83,6 +83,7 @@ def make_configs(
 
     pull_commands = script_head.copy()
     start_pod_commands = script_head.copy()
+    create_container_commands = script_head.copy()
     start_container_commands = script_head.copy()
     check_commands = script_head.copy()
     stop_container_commands = script_head.copy()
@@ -144,16 +145,10 @@ def make_configs(
         group_memory = 0
 
         pod_name = f"{prefix}_{container_group_id}"
+        pod_json_file = f"pod_{container_group_id}.json)"
 
-        start_pod_commands.append(
-            " ".join(
-                [
-                    "$group_id = (crictl runp",
-                    "--runtime runhcs-lcow",
-                    f"./pod_{container_group_id}.json)",
-                ]
-            )
-        )
+        start_pod_commands.append(f"$group_id = (crictl runp --runtime runhcs-lcow {pod_json_file})")
+        start_pod_commands.append(f"Write-Output 'Started pod {pod_name} with ID: ' $group_id")
 
         shimdiag_exec_pod = f'shimdiag_exec_pod -podName "{pod_name}" --'
 
@@ -272,7 +267,6 @@ def make_configs(
                         {"key": env["name"], "value": env["value"]}
                         for env in container["properties"]["environmentVariables"]
                     ]
-                container_json["log_path"] = f"C:\\{prefix}\\container_log_{container_group_id}_{container_id}.log"
                 if "securityContext" in container["properties"]:
                     container_sec_ctx = container["properties"]["securityContext"]
                     if container_sec_ctx.get("privileged", False):
@@ -281,19 +275,45 @@ def make_configs(
                 json.dump(container_json, f, indent=2)
 
             # Create Container
-            start_container_commands.append(
+            container_json_file = f"container_{container_group_id}_{container_id}.json"
+            container_log_filename = f"container_log_{container_group_id}_{container_id}.log"
+
+            create_container_commands.append("# Fix log_path to use correct absolute path")
+            create_container_commands.append(
+                f"$containerJson = Get-Content {container_json_file} | ConvertFrom-Json"
+            )
+            # Using .log_path raises "The property 'log_path' cannot be found on this object".
+            # Using ["log_path"] raises "Unable to index into an object of type "System.Management.Automation.PSObject"."
+            create_container_commands.append(
+                f'Add-Member -InputObject $containerJson -NotePropertyName log_path -NotePropertyValue "$PWD\\{container_log_filename}" -Force'
+            )
+
+            create_container_commands.append(
+                f'$containerJson | ConvertTo-Json -Depth 100 | Set-Content -Encoding utf8 {container_json_file}'
+            )
+            create_container_commands.append("")
+
+            create_container_commands.append(
                 " ".join(
                     [
-                        "$container_id = (crictl create --no-pull",
+                        "crictl create --no-pull",
                         f"(crictl pods --name {pod_name} -q)",
-                        f"./container_{container_group_id}_{container_id}.json",
-                        f"./pod_{container_group_id}.json)",
+                        container_json_file,
+                        pod_json_file,
                     ]
                 )
             )
 
             # Start Container
-            start_container_commands.append("crictl start $container_id")
+            start_container_commands.append(
+                f"$containerId=(crictl ps --pod (crictl pods --name {pod_name} -q) --name {container_name} -q -a)"
+            )
+            start_container_commands.append("if (!$containerId) {")
+            start_container_commands.append(f"  Write-Error 'Container {container_name} not created yet.  Run createc.ps1.'")
+            start_container_commands.append("} else {")
+            start_container_commands.append("  crictl start $containerId")
+            start_container_commands.append(f"  Write-Output 'Started container {container_name} with ID: ' $containerId")
+            start_container_commands.append("}")
 
             check_commands.append(
                 f"$res=(container_exec -podName {pod_name} -containerName {container_name} -- "
@@ -311,7 +331,7 @@ def make_configs(
             stop_container_commands.append(
                 "; ".join(
                     [
-                        f"$containerId=crictl ps --pod (crictl pods --name {pod_name} -q) --name {container_name} -q -a",
+                        f"$containerId=(crictl ps --pod (crictl pods --name {pod_name} -q) --name {container_name} -q -a)",
                         "if ($containerId) { crictl stop -t 10 $containerId",
                         "crictl rm $containerId }",
                     ]
@@ -358,6 +378,7 @@ def make_configs(
 
     write_script("pull.ps1", pull_commands)
     write_script("runp.ps1", start_pod_commands)
+    write_script("createc.ps1", create_container_commands)
     write_script("startc.ps1", start_container_commands)
     write_script(
         "check.ps1",
@@ -391,6 +412,7 @@ def make_configs(
             ".\\pull.ps1",
             ".\\stop.ps1",
             ".\\runp.ps1",
+            ".\\createc.ps1",
             ".\\startc.ps1",
             ".\\check.ps1",
         ],
